e6cb8c3428424936587d4a1513f9b60e56b3eaff
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-11-01 17:57:19 +0500

    BrickBreaker Game with Skia

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { Canvas } from "@shopify/react-native-skia";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Canvas style={{ flex: 1 }}>
+        <View></View>
+      </Canvas>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  titleContainer: {
+    flexDirection: "row",
+  },
+  titleTextNormal: {
+    color: "white",
+    fontSize: 40,
+  },
+  titleTextBold: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});
---
+++ README.md
@@ -0,0 +1,9 @@
+# Game Dev with Expo and Skia!
+
+A Brick Breaker game created using purely React-Native and Skia with Reanimated .
+
+## How to run it:
+
+1. `cd` into the project
+2. Run the `yarn` command
+3. Run `npx expo start` and open on the device of your choice
